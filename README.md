165e8d59f5b9fe0b8c62cb32f300744d8e1b2b55
arnovandash <arnovandash@gmail.com>
2020-07-05 16:58:51 +0200

    implemented chip8 memory

--- README.md
+++ README.md
@@ -6,5 +6,7 @@ A CHIP-8 emulator written in C
 Download SDL2-devel-2.0.12-mingw.tar
 
 cp SDL2-2.0.12/bin/SDL2.dll ~/chip8/bin/
+
 cp -r SDL2-2.0.12/include/SDL2 ~/chip8/include/
+
 cp -r SDL2-2.0.12/lib ~/chip8/
--- src/main.c
+++ src/main.c
@@ -1,17 +1,50 @@
 #include <stdio.h>
-#include "../include/SDL2/SDL.h"
+#include "SDL2/SDL.h"
+#include "chip8.h"
 
-int WinMain(int argc, char** argv) {
+int WinMain(int argc, char **argv)
+{
+    struct chip8 chip8;
+    chip8_memory_set(&chip8.memory, 0xffffffff, 'z');
+    printf("%c\n", chip8_memory_get(&chip8.memory, 0x400));
+
+
     SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Window* window = SDL_CreateWindow(
-        "CHIP8 Window",
+    SDL_Window *window = SDL_CreateWindow(
+        EMULATOR_WINDOW_TITLE,
         SDL_WINDOWPOS_UNDEFINED,
         SDL_WINDOWPOS_UNDEFINED,
-        640, 320, SDL_WINDOW_SHOWN
-    );
-    printf("I am a chip8 emulator!\n");
+        CHIP8_WIDTH * CHIP8_WINDOW_MULTIPLIER,
+        CHIP8_HEIGHT * CHIP8_WINDOW_MULTIPLIER,
+        SDL_WINDOW_SHOWN);
 
-    while(1){}
-    SDL_DestroyWindow(window);    
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_TEXTUREACCESS_TARGET);
+    while(1)
+    {
+        SDL_Event event;
+        while(SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+            {
+                goto out;
+            }
+            SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+            SDL_RenderClear(renderer);
+            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0);
+            /*
+            SDL_Rect r;
+            r.x = 0;
+            r.y = 0;
+            r.w = 50;
+            r.h = 50;
+            SDL_RenderFillRect(renderer, &r);
+            */
+            SDL_RenderPresent(renderer);
+
+        }
+    }
+
+out:
+    SDL_DestroyWindow(window);
     return 0;
 }
